675f247d0924481961d6213c28288c802af95703
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2024-04-07 19:02:28 +0000

    Run tests

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
+cd mediawiki
+git clean -fxd
+cd ..
 cp .env mediawiki
 echo "MW_DOCKER_UID=$(id -u)
 MW_DOCKER_GID=$(id -g)" >> mediawiki/.env
 unlink $PWD/mediawiki/skins/Vector
-ln -s $PWD/Vector $PWD/mediawiki/skins/Vector
-unlink $PWD/mediawiki/extensions/Math
-ln -sf $PWD/Math $PWD/mediawiki/extensions/Math
+cp -r $PWD/Vector $PWD/mediawiki/skins/Vector
+cp -r $PWD/Math $PWD/mediawiki/extensions/Math
 cd mediawiki
 docker-compose up -d
+docker-compose exec mediawiki composer update
+docker-compose exec mediawiki /bin/bash /docker/install.sh
+echo "wfLoadSkin( 'Vector' );
+wfLoadExtension( 'Math' );" >> LocalSettings.php
+docker-compose exec mediawiki php maintenance/run.php update.php --quick
+docker-compose exec mediawiki composer phpunit:entrypoint -- ./extensions/Math/tests/phpunit/
