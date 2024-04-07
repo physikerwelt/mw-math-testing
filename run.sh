f3f2cd35fb166984749246cd21cb343c41367857
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2024-04-07 17:26:55 +0000

    Add links to extensions

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 cp .env mediawiki
 echo "MW_DOCKER_UID=$(id -u)
 MW_DOCKER_GID=$(id -g)" >> mediawiki/.env
+unlink $PWD/mediawiki/skins/Vector
+ln -s $PWD/Vector $PWD/mediawiki/skins/Vector
+unlink $PWD/mediawiki/extensions/Math
+ln -sf $PWD/Math $PWD/mediawiki/extensions/Math
 cd mediawiki
 docker-compose up -d
