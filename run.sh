80d07d0383fbdcc35fdfe57dd9368162fe2213fb
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2024-04-08 11:24:23 +0000

    Run tests before curly removal

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ cd ..
 cp .env mediawiki
 echo "MW_DOCKER_UID=$(id -u)
 MW_DOCKER_GID=$(id -g)" >> mediawiki/.env
-unlink $PWD/mediawiki/skins/Vector
 cp -r $PWD/Vector $PWD/mediawiki/skins/Vector
 cp -r $PWD/Math $PWD/mediawiki/extensions/Math
 cd mediawiki
@@ -14,4 +13,5 @@ docker-compose exec mediawiki /bin/bash /docker/install.sh
 echo "wfLoadSkin( 'Vector' );
 wfLoadExtension( 'Math' );" >> LocalSettings.php
 docker-compose exec mediawiki php maintenance/run.php update.php --quick
-docker-compose exec mediawiki composer phpunit:entrypoint -- ./extensions/Math/tests/phpunit/
+docker-compose exec mediawiki /bin/bash -c 'cd /var/www/html/w/extensions/Math/maintenance && ./downloadMoreTexVCtests.sh'
+docker-compose exec mediawiki /bin/bash -c 'export COMPOSER_PROCESS_TIMEOUT=3600 && composer phpunit -- ./extensions/Math/tests/phpunit/'
