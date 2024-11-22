f0d152811d99ea7da782fd7f1520274fca41152b
Moritz Schubotz <github@ckurs.de>
2024-11-22 22:46:02 +0100

    Switch from docker-compose to docker compose

--- run.sh
+++ run.sh
@@ -7,11 +7,11 @@ MW_DOCKER_GID=$(id -g)" >> mediawiki/.env
 cp -r $PWD/Vector $PWD/mediawiki/skins/Vector
 cp -r $PWD/Math $PWD/mediawiki/extensions/Math
 cd mediawiki
-docker-compose up -d
-docker-compose exec mediawiki composer update
-docker-compose exec mediawiki /bin/bash /docker/install.sh
+docker compose up -d
+docker compose exec mediawiki composer update
+docker compose exec mediawiki /bin/bash /docker/install.sh
 echo "wfLoadSkin( 'Vector' );
 wfLoadExtension( 'Math' );" >> LocalSettings.php
-docker-compose exec mediawiki php maintenance/run.php update.php --quick
-docker-compose exec mediawiki /bin/bash -c 'cd /var/www/html/w/extensions/Math/maintenance && ./downloadMoreTexVCtests.sh'
-docker-compose exec mediawiki /bin/bash -c 'export COMPOSER_PROCESS_TIMEOUT=3600 && composer phpunit -- ./extensions/Math/tests/phpunit/unit'
+docker compose exec mediawiki php maintenance/run.php update.php --quick
+docker compose exec mediawiki /bin/bash -c 'cd /var/www/html/w/extensions/Math/maintenance && ./downloadMoreTexVCtests.sh'
+docker compose exec mediawiki /bin/bash -c 'export COMPOSER_PROCESS_TIMEOUT=3600 && composer phpunit -- ./extensions/Math/tests/phpunit/unit'
