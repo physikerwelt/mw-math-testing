7e4b515e3bb9f2b0bce508c4016d3d0e84c9e844
physikerwelt <wiki@physikerwelt.de>
2025-06-01 07:49:28 +0200

    Fix remove superflous install in prepare-more-tests.sh

--- prepare-more-tests.sh
+++ prepare-more-tests.sh
@@ -7,7 +7,6 @@ URL=https://zenodo.org/records/15162182/files/wmf_texvc_inputs.json
 cp -r $PWD/MathSearch $PWD/mediawiki/extensions/MathSearch
 cd mediawiki || exit 1
 docker compose exec mediawiki composer update
-docker compose exec mediawiki /bin/bash /docker/install.sh
 echo "wfLoadExtension( 'MathSearch' );" >> LocalSettings.php
 docker compose exec mediawiki php maintenance/run.php update.php --quick
 docker compose exec mediawiki /bin/bash -c 'export COMPOSER_PROCESS_TIMEOUT=3600 && composer phpunit -- /var/www/html/w/extensions/MathSearch/tests/phpunit/unit'
