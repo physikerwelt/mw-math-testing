51bdcbacca7d889fa2a84d054ea1505906f4cc9e
physikerwelt <wiki@physikerwelt.de>
2025-06-04 07:01:20 +0200

    Run integration tests with dop 3

--- performance.sh
+++ performance.sh
@@ -3,4 +3,4 @@
 set -euxo pipefail
 
 cd mediawiki || exit 1
-docker compose exec mediawiki /bin/bash -c 'cd /var/www/html/w/extensions/MathSearch/maintenance/ && ./performance.sh 2'
+docker compose exec mediawiki /bin/bash -c 'cd /var/www/html/w/extensions/MathSearch/maintenance/ && ./performance.sh 3'
